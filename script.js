8669d15c78e585ff5f72f37009302890fc8c8621
Abuye Mamuye <abuyeye3@gmail.com>
2021-01-12 19:31:38 -0800

    added the localstorage into the script js

--- script.js
+++ script.js
@@ -1,46 +1,56 @@
+//todo 
+//save lisener localstorage
+//if else statment for color
 
 $(document).ready(function () {
 
-   var times = [9, 10, 11, 12, 1, 2, 3, 4,5];
-    for (var i =0; i < times.length; i++) {
-    var dataHour = localStorage.getItem(times[i]);
-    $(".form" + times[i]).val(dataHour);
-    //console.log("times");
-}
+localStorage.getItem(time)  
+  var time;
+  var task; 
+$(".saveBtn").click(function() {
+   //value of text box, set in local storage
+console.log($(this).siblings("input").val()) 
+//returns number for the hour
+console.log($(this).parent().attr("data-hour"))
+time = $(this).parent().attr("data-hour");
+task = $(this).siblings("input").val();
+//saves in to local storage
+localStorage.setItem(time,task);
 
 
-// event listener for localstorage
-    $(".saveBtn").click(function() {
-    event.preventDefault();
-    var formValue = $(this).siblings(".form-control").val()
-    $(".form" + times[i]).val(dataHour);
-    //console.log("helo");
 })
-    
-
-
-
-//console.log("hello");
+  
 // A function the current time and date
     
-var update = function (date) {
+     var update = function (date) {
         $("#currentDay").html(moment().format('MMMM Do YYYY, h:mm:ss a'));
    var currentTime = moment();
    currentTime = currentTime.startOf("hour");
    var beforeTime = moment().startOf("day").add(9, "hours");
 
-              var timeBlock1 =beforeTime.add(0, "h");
-              timeBlock1 = timeBlock1.format("hh:mm A");
-              $("#form9").text(timeBlock1);
+          
 
     }
 
    setInterval(update, 1000);
    
- 
+ //display local storage data to screen
+ $("#form9").val(localStorage.getItem("9"))
+ $("#form10").val(localStorage.getItem("10"))
+ $("#form11").val(localStorage.getItem("11"))
+ $("#form12").val(localStorage.getItem("12"))
+ $("#form1").val(localStorage.getItem("1"))
+ $("#form2").val(localStorage.getItem("2"))
+ $("#form3").val(localStorage.getItem("3"))
+ $("#form4").val(localStorage.getItem("4"))
+ $("#form5").val(localStorage.getItem("5"))
+
  
 
+ 
+ 
 });
+
 
 
 

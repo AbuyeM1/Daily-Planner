a13545391fac55a34d090f086123eda73a0809e4
Abuye Mamuye <abuyeye3@gmail.com>
2021-01-14 16:38:54 -0800

    made change some file

--- script.js
+++ script.js
@@ -4,51 +4,65 @@
 
 $(document).ready(function () {
 
-localStorage.getItem(time)  
-  var time;
-  var task; 
-$(".saveBtn").click(function() {
-   //value of text box, set in local storage
-console.log($(this).siblings("input").val()) 
-//returns number for the hour
-console.log($(this).parent().attr("data-hour"))
-time = $(this).parent().attr("data-hour");
-task = $(this).siblings("input").val();
-//saves in to local storage
-localStorage.setItem(time,task);
+  // A function the current time and date
 
-
-})
+  var update = function (date) {
+    $("#currentDay").html(moment().format('MMMM Do YYYY, h:mm:ss a'));
+    var currentTime = moment();
+    currentTime = currentTime.startOf("hour");
+    var beforeTime = moment().startOf("day").add(9, "hours");
+  }
+  setInterval(update, 1000);
   
-// A function the current time and date
+  localStorage.getItem(time)
+  var time;
+  var task;
+  $(".saveBtn").click(function () {
+    //value of text box, set in local storage
+    console.log($(this).siblings("input").val())
+    //returns number for the hour
+    console.log($(this).parent().attr("data-hour"))
+    time = $(this).parent().attr("data-hour");
+    task = $(this).siblings("input").val();
+    //saves in to local storage
+    localStorage.setItem(time, task);
+
+
+  })
+  function testTime() {
+    time9 = moment().startOf('day').add(9, "hours");
+    currentTime = currentTime.startOf("hours");
     
-     var update = function (date) {
-        $("#currentDay").html(moment().format('MMMM Do YYYY, h:mm:ss a'));
-   var currentTime = moment();
-   currentTime = currentTime.startOf("hour");
-   var beforeTime = moment().startOf("day").add(9, "hours");
-
-          
-
-    }
-
-   setInterval(update, 1000);
+    if (currentTime.isAfter(time9)) {
+      $(".form-control").addClass("past");
+    } else if (currentTime.isBefor(time9)) {
+      $(".form-control").addClass("future");
+    } else if (currentTime.isSame(time9)) {
+      $(".form-control").addClass("present");
+    };
    
- //display local storage data to screen
- $("#form9").val(localStorage.getItem("9"))
- $("#form10").val(localStorage.getItem("10"))
- $("#form11").val(localStorage.getItem("11"))
- $("#form12").val(localStorage.getItem("12"))
- $("#form1").val(localStorage.getItem("1"))
- $("#form2").val(localStorage.getItem("2"))
- $("#form3").val(localStorage.getItem("3"))
- $("#form4").val(localStorage.getItem("4"))
- $("#form5").val(localStorage.getItem("5"))
 
- 
+  }
 
- 
- 
+//display local storage data to screen
+  $("#form9").val(localStorage.getItem("9"));
+
+  $("#form10").val(localStorage.getItem("10"));
+
+  $("#form11").val(localStorage.getItem("11"));
+
+  $("#form12").val(localStorage.getItem("12"));
+
+  $("#form1").val(localStorage.getItem("1"));
+
+  $("#form2").val(localStorage.getItem("2"));
+
+  $("#form3").val(localStorage.getItem("3"));
+
+  $("#form4").val(localStorage.getItem("4"));
+
+  $("#form5").val(localStorage.getItem("5"));
+
 });
 
 

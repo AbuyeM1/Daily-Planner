76d295ad66f17c56bf7866e215de327bdcc6a9e6
Abuye Mamuye <abuyeye3@gmail.com>
2021-01-14 22:44:47 -0800

    modified the script.js and index.html

--- script.js
+++ script.js
@@ -5,7 +5,6 @@
 $(document).ready(function () {
 
   // A function the current time and date
-
   var update = function (date) {
     $("#currentDay").html(moment().format('MMMM Do YYYY, h:mm:ss a'));
     var currentTime = moment();
@@ -13,38 +12,49 @@ $(document).ready(function () {
     var beforeTime = moment().startOf("day").add(9, "hours");
   }
   setInterval(update, 1000);
-  
+
   localStorage.getItem(time)
   var time;
   var task;
   $(".saveBtn").click(function () {
+
     //value of text box, set in local storage
     console.log($(this).siblings("input").val())
+
     //returns number for the hour
     console.log($(this).parent().attr("data-hour"))
     time = $(this).parent().attr("data-hour");
     task = $(this).siblings("input").val();
+
     //saves in to local storage
     localStorage.setItem(time, task);
 
 
   })
-  function testTime() {
-    time9 = moment().startOf('day').add(9, "hours");
-    currentTime = currentTime.startOf("hours");
-    
-    if (currentTime.isAfter(time9)) {
-      $(".form-control").addClass("past");
-    } else if (currentTime.isBefor(time9)) {
-      $(".form-control").addClass("future");
-    } else if (currentTime.isSame(time9)) {
-      $(".form-control").addClass("present");
-    };
-   
+  // for color function
+  function updateColors() {
+    var currentTime = new  moment().hour();
+    console.log(currentTime);
+  
+    $(".input-group-lg").each(function(){
+      console.log($(this).attr("data-hour"));
+      var dataHour=$(this).attr("data-hour");
+      
+      if(dataHour<currentTime){
+          $(this).children('input').attr("class","form-control past");
+      }
+      else if(dataHour==currentTime){
+        $(this).children('input').attr("class","form-control present");
+      }
+      else{
+        $(this).children('input').attr("class","form-control future");
+      }
 
+    })
   }
+  
 
-//display local storage data to screen
+  //display local storage data to screen
   $("#form9").val(localStorage.getItem("9"));
 
   $("#form10").val(localStorage.getItem("10"));
@@ -62,6 +72,9 @@ $(document).ready(function () {
   $("#form4").val(localStorage.getItem("4"));
 
   $("#form5").val(localStorage.getItem("5"));
+
+
+  updateColors();
 
 });
 
